c0f2a57ff3976d6634a81372ca91afafb6330624
JoseOgam <jotienoochieng@gmail.com>
2021-07-21 16:23:54 -0400

    modified addTask.js file

--- task-app/src/components/addTask.js
+++ task-app/src/components/addTask.js
@@ -1,8 +1,21 @@
-let AddTask = () => {
+import React, { useState } from 'react'
+let AddTask = (props) => {
+    let [title, setTitle] = useState('')
+    let [body, setBody] = useState('')
+    let addTask = (e) => {
+        e.preventDefault()
+        props.dispatch({
+            type: "ADD_TASK",
+            title,
+            body
+        })
+        setTitle('');
+        setBody('');
+    }
     return (<div>
-        <form>
-            <input placeholder="title" />
-            <textarea placeholder="body" />
+        <form onSubmit={addTask}>
+            <input value={title} placeholder="title" onChange={(e)=>setTitle(e.target.value)} />
+            <textarea value={body} placeholder="body" onChange={(e)=>setBody(e.target.value)} />
             <button>Submit</button>
         </form>
     </div>)
