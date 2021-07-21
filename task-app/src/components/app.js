f7f37f570e520de94cf0eaa3ce7871b3acf6b0a4
JoseOgam <jotienoochieng@gmail.com>
2021-07-21 16:24:25 -0400

    modified app.js file

--- task-app/src/components/app.js
+++ task-app/src/components/app.js
@@ -1,11 +1,14 @@
+import React, { useReducer } from "react"
 import AddTask from "./addTask"
 import ListTask from "./listTask"
+import reducer from "./reducer/reducer"
 
 let App = () => {
+    let [tasks, dispatch] = useReducer(reducer,[])
     return (<div>
         <h1>Task App</h1>
-        <ListTask />
-        <AddTask />
+        <ListTask tasks={tasks} />
+        <AddTask dispatch={dispatch}/>
     
     </div>)
 }
