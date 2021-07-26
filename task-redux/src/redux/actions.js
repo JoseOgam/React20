d266c49813c0e1fe46319590752d0ffeb8f092e3
JoseOgam <jotienoochieng@gmail.com>
2021-07-26 14:30:07 -0400

    react redux todos api

--- task-redux/src/redux/actions.js
+++ task-redux/src/redux/actions.js
@@ -1,12 +1,33 @@
-const incrementAction = {
-    type: 'INCREMENT'
+import * as actionTypes from './constants'
+
+export const addTodos = () => {
+    return {
+        type: actionTypes.ADD_TODOS
+    }
+}
+export const setTitle = (title) => {
+    return {
+        type: actionTypes.SET_TITLE,
+        title: title
+    }
 }
 
-const decrementAction = {
-    type: 'DECREMENT'
+export const showTodos = () => {
+    return {
+        type: actionTypes.SHOW_TODOS
+    }
 }
 
-export default {
-    incrementAction,
-    decrementAction
+export const removeTodos = (todo) => {
+    return {
+        type: actionTypes.REMOVE_TODOS,
+        todo: todo
+    }
+}
+
+export const setError = (error) => {
+    return {
+        type: actionTypes.SET_ERROR,
+        error: error
+    }
 }
