66c78c07a1c35a33ef1f540951e874a3b83d2194
JoseOgam <jotienoochieng@gmail.com>
2021-07-27 18:16:37 -0400

    installed dependencies and created files

--- modals/src/App.js
+++ modals/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ReactModal from './components/modal';
+
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  function toggleModal() {
+    setIsOpen(!isOpen)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <button onClick={toggleModal} >open modal</button>
+      <ReactModal toggleModal={toggleModal} isOpen={isOpen} />
     </div>
   );
 }
