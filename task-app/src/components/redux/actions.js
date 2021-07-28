539753447a13441fdf67d304f9a8f7d17bde4413
JoseOgam <jotienoochieng@gmail.com>
2021-07-28 13:14:20 -0400

    added acton.js file

---
+++ task-app/src/components/redux/actions.js
@@ -0,0 +1,34 @@
+import * as actionType from './constants'
+
+export const addTask = () => {
+    return {
+        type: actionType.ADD_TASK
+    }
+}
+
+export const setTitle = (title) => {
+    return {
+        type: actionType.SET_TITLE,
+        title: title
+    }
+}
+
+export const setBody = (body) => {
+    return {
+        type: actionType.SET_BODY,
+        body: body
+    }
+}
+
+export const setError = (error) => {
+    return {
+        type: actionType.SET_ERROR,
+        error: error
+    }
+}
+
+export const showTasks = () => {
+    return {
+        type: actionType.SHOW_TASKS
+    }
+}
