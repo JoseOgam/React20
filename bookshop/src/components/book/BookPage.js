78d26c1fa812b4a89698f8c5b392e1ce1d9e8f87
JoseOgam <jotienoochieng@gmail.com>
2021-12-07 07:34:47 -0500

    fundamentals of react-router-dom

--- bookshop/src/components/book/BookPage.js
+++ bookshop/src/components/book/BookPage.js
@@ -1,6 +1,21 @@
 const Book = () => {
     return (<div>
-        
+        <h2>Our Books Store</h2>
+        <div>
+            <ol>
+                <li>The Alligator's Toothache</li>
+                <li>Amber Brown</li>
+                <li>American Girl</li>
+                <li>Americans Before Columbus</li>
+                <li>An American Plague</li>
+                <li>Animal Fairy Tales</li>
+                <li>The Anubis Tapestry</li>
+                <li>Arilla Sun Down</li>
+                <li>Arnie the Doughnut</li>
+                <li>Away West</li>
+            </ol>
+        </div>
+
     </div>)
 }
 export default Book
