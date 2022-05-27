1e989434e3c471602941677928e7e65a979aee93
JoseOgam <jotienoochieng@gmail.com>
2022-05-27 11:40:21 +0300

    componet concept

---
+++ socket-client/src/components/counter.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+const Counter = () => {
+     const [counter, setCounter] = useState(0)
+
+  const add = () => setCounter(counter + 1)
+  const subtract = () => setCounter(counter - 1)
+  const reset = () => setCounter(0)
+    return (<div>
+       
+
+      <div>{counter}</div>
+      <button onClick={add} >add</button>
+      <button onClick={reset} >reset</button>
+      <button onClick={subtract} >subtract</button>
+    
+    </div>)
+}
+
+export default Counter
