18e3a1583021d635d214d13331e74ba5dad959ce
JoseOgam <jotienoochieng@gmail.com>
2023-04-04 15:38:20 +0300

    localStorage functionality updated

--- lists/src/components/tasks.js
+++ lists/src/components/tasks.js
@@ -1,16 +1,16 @@
-import {connect} from "react-redux"
+// import {connect} from "react-redux"
 
-const Tasks = (props) => {
-    return (<div>
-        <h3>{ props.task.title }</h3>
-        <p>{props.task.body}</p>
-    </div>)
-}
+// const Tasks = (props) => {
+//     return (<div>
+//         <h3>{ props.task.title }</h3>
+//         <p>{props.task.body}</p>
+//     </div>)
+// }
 
-const mapStateToProps = (state) => {
-    return {
-        listTasks: state.tasks
-    }
-}
+// const mapStateToProps = (state) => {
+//     return {
+//         listTasks: state.tasks
+//     }
+// }
 
-export default connect(mapStateToProps)(Tasks);
+// export default connect(mapStateToProps)(Tasks);
