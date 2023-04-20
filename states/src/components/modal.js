72df94f712efa94a0c6e2801afe4f7a1f4008801
JoseOgam <jotienoochieng@gmail.com>
2023-04-20 16:17:14 +0300

    React Redux CRUD

--- states/src/components/modal.js
+++ states/src/components/modal.js
@@ -9,32 +9,53 @@ const Modal = ({ setIsOpen }) => {
         <div>
             <>
                 <div className={ styles.darkBG } onClick={ () => setIsOpen(false) } />
-                <div className={styles.centered}>
-                <div className={styles.modal}>
-                 <div className={styles.modalHeader}>
-                 <h5 className={styles.heading}>Dialog</h5>
-                </div>
-                 <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
-                 <RiCloseLine style={{ marginBottom: "-3px" }} />
-                </button>
-                <div className={styles.modalContent}>
-                            <input placeholder="title" value={todos.title} />
-                            <input placeholder="body" value={todos.body}/>
-                </div>
-                 <div className={styles.modalActions}>
-                 < div className={ styles.actionsContainer }>
-                    <button
-                className={styles.cancelBtn}
-                onClick={() => setIsOpen(false)}
-              >
-                Cancel
-            </button>
-              <button className={styles.updateBtn} onClick={() => setIsOpen(false)}>
-                Update
-              </button>
-            </div>
-           </div>
-             </div>
+
+                <div className={ styles.centered }>
+                    
+                    <div className={ styles.modal }>
+                        
+                        <div className={ styles.modalHeader }>
+                            
+                            <h5 className={ styles.heading }>Dialog</h5>
+                            
+                        </div>
+                        
+                        <button className={ styles.closeBtn } onClick={ () => setIsOpen(false) }>
+                            
+                            <RiCloseLine style={ { marginBottom: "-3px" } } />
+                            
+                        </button>
+                        
+                        <div className={ styles.modalContent }>
+
+                            
+                            <input placeholder="title" value={ todos.title } />
+                            
+                            <input placeholder="body" value={ todos.body } />
+                            
+                        </div>
+                        
+                        <div className={ styles.modalActions }>
+                            
+                            < div className={ styles.actionsContainer }>
+                                
+                                <button className={ styles.cancelBtn } onClick={ () => setIsOpen(false) } >
+                                    
+                                Cancel
+            
+                                </button>
+
+                        < button className={styles.updateBtn} onClick={() => setIsOpen(false)}>
+                                    Update
+                                    
+                                </button>
+                                
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    
             </div>
             </>
         </div>
