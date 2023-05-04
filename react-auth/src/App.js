26f2a9621a11574d7fa623d6c1bfc4ffeaf6cb15
JoseOgam <jotienoochieng@gmail.com>
2023-05-04 11:59:16 +0300

    react auth app

--- react-auth/src/App.js
+++ react-auth/src/App.js
@@ -1,19 +1,7 @@
-import About from "./components/about";
-import Contact from "./components/contacts";
-import Feedback from "./components/feedback";
-import Home from "./components/home";
-import { Routes, Route } from 'react-router-dom';
 function App() {
   return(
      <div className="App">
-      <Routes>
-        <Route path="/" element={< Home />} />
-        <Route path="contact" element={< Contact />} />
-        <Route path="about" element={< About />}>
-          <Route to="feedback" element={< Feedback />}/>
-        </Route>
-        <Route path="*" element={<h2>404 error the page you looking does not exist</h2>} />
-      </Routes>
+     
      </div>
   )
   
