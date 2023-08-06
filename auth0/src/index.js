b0bd7fb79e1e12feacf4321688f6a49f8ad70a8b
Joseph <joseph@HP-EliteBook-820-G3-c90a9ed2.localdomain>
2023-08-06 23:45:09 +0300

    configuring AuTH0 into react App

--- auth0/src/index.js
+++ auth0/src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientid = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientid}
+    redirecturi={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
