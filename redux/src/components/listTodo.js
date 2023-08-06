07e19d7c446e97a5477a348d91531c0c4bc8586e
Joseph <joseph@HP-EliteBook-820-G3-c90a9ed2.localdomain>
2023-08-06 21:15:39 +0300

    Todo components

--- redux/src/components/listTodo.js
+++ redux/src/components/listTodo.js
@@ -16,10 +16,41 @@ const ListTodo = () => {
     setEditing(true);
     setState({ ...state, id, content });
   };
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+      [`${e.target.name}Error`]: null,
+    });
+  };
+
+  const { content, contentError, id } = state;
+  const edit = () => {
+    if (content === "") {
+      setState({ ...state, contentError: "You must write something!" });
+      return;
+    }
+    dispatch(editTodo({ content, id }));
+    setEditing(false);
+  };
 
   return (
     <div>
-      {
+      {isEditing ? (
+        <div className="form">
+          <h2>Update your plan for today</h2>
+          <input
+            type="text"
+            value={content}
+            name="content"
+            onChange={handleChange}
+          ></input>
+          <button type="button" className="button" onClick={edit}>
+            Edit
+          </button>
+          {contentError ? <div className="error">{contentError}</div> : null}
+        </div>
+      ) : (
         <ul className="todos">
           {todoList.map(({ id, content }) => {
             return (
@@ -39,7 +70,7 @@ const ListTodo = () => {
             );
           })}
         </ul>
-      }
+      )}
     </div>
   );
 };
