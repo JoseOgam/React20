fef7801fe34cabef67b3d54006695c6547717797
Joseph <joseph@HP-EliteBook-820-G3-c90a9ed2.localdomain>
2023-09-12 09:27:09 +0300

    initial commit

---
+++ e-stores/src/components/redux/features/slice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addTask: (state, action) => {
+      let newTask = {
+        id: Date.now(),
+        task: action.payload.task,
+      };
+      state.items.push(newTask);
+    },
+    deleteTask: (state, action) => {
+      return state.items.filter((item) => item.id !== action.payload.id);
+    },
+  },
+});
+const { addTask, deleteTask } = taskSlice.actions;
+const taskReducer = taskSlice.reducer;
+export default taskReducer;
