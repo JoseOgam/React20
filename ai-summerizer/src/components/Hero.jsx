ed75ca55af745fb09f56beb50577cf59e7ad28c8
Joseph <joseph@HP-EliteBook-820-G3-c90a9ed2.localdomain>
2023-09-19 22:32:36 +0300

    hero component

--- ai-summerizer/src/components/Hero.jsx
+++ ai-summerizer/src/components/Hero.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { copy } from "../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full  justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={copy} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/JoseOgam", "_blank")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summerize your Articals with <br className="max-md: hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        simplify your reading with summarizer, lengthy articles are transformed
+        in clear and concise summary
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
