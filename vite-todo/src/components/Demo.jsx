54faf50f28941e6e2c8bf799b4b33c59daee40f6
Joseph <jotienoochieng@gmail.com>
2023-10-01 06:37:22 +0300

    components and styles setup

--- vite-todo/src/components/Demo.jsx
+++ vite-todo/src/components/Demo.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  return (
+    <section className="w-full mt-16 max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form className="flex justify-center items-center">
+          <input type="text" placeholder="write your todo..." />
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
--- vite-todo/src/components/Hero.jsx
+++ vite-todo/src/components/Hero.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
+import { logo } from "../assets";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full justify-center items-center flex-col">
+      <nav className="flex justify-between items-center mb-10 pt-3">
+        <img src={logo} alt="todo logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/JoseOgam", "_blank")}
+          className="black_btn"
+        >
+          github
+        </button>
+      </nav>
+      <div>
+        <h1 className="head_text">
+          Simplify your <br className="max-md: hidden" />
+          <span> daily Schedule</span>
+        </h1>
+      </div>
+    </header>
+  );
 };
 
 export default Hero;
