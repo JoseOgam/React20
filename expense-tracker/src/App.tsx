41deaab61a5865300c6aed468efed35bf848dec8
Joseph <jotienoochieng@gmail.com>
2024-04-03 07:38:50 +0300

    first commit

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <div className="flex  items-center justify-center underline">
+        expense App
+      </div>
+    </div>
+  );
+};
+
+export default App;
