b06fdb9fe7ad0f061e0e6072412cb7a5187ff146
Joseph <jotienoochieng@gmail.com>
2024-08-27 18:39:04 +0300

    initial commit

--- d.s/ls.js
+++ d.s/ls.js
@@ -10,11 +10,38 @@ class LinkedList {
     this.head = null;
   }
 
-  append() {}
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
   prepend() {}
   delete() {}
   search() {}
-  print() {}
+  print() {
+    let current = this.head;
+    let element = [];
+    while (current != null) {
+      element.push(current.data);
+      current = current.next;
+    }
+    console.log(element.join("->"));
+  }
 }
 
 const list = new LinkedList();
+
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+list.append(50);
+
+list.print();
